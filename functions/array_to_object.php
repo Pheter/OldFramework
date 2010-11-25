f310449af6f13f7a30606191a627a8cd7fc0f7ba
Peter Horne <peterhorne92@gmail.com>
2010-11-25 23:02:28 +0000

    Added $request array.

--- functions/array_to_object.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function array_to_object($array) {
-    
-    return is_array($array) ? (object)array_map(__FUNCTION__, $array) : $array;
-}
--- init.php
+++ init.php
@@ -1,12 +1,7 @@
 <?php
 
-//Include generic functions
-require('functions/array_to_object.php');
-
 //Enable autoload
-spl_autoload_register(
-    
-    function($class_name) {
+spl_autoload_register(function($class_name) {
         
         $class_name = explode('\\', $class_name);
         
@@ -26,6 +21,29 @@ $Spyc = new Spyc;
 //Load settings
 $Settings = $Spyc->loadFile('config.yml');
 
+//Create $request array
+$url = parse_url($_SERVER['REQUEST_URI']);
+$request['path'] = $url['path'];
+$request['method'] = strtoupper($_SERVER['REQUEST_METHOD']);
+
+//HTML form fix (enable usage of methods other than GET and POST)
+if ($request['method'] == 'POST') {
+    if (isset($_POST['_method'])) {
+        $request['method'] = strtoupper($_POST['_method']);
+    }
+}
+
+switch ($request['method']) {
+    case 'GET':
+        $request['data'] = $_GET;
+        break;
+    default:
+        parse_str(file_get_contents('php://input'), $request['data']);
+        break;
+}
+
+unset($request['data']['_method']);
+
 //Build Plugins
 require('Plugins.php');
 $Plugins = new Seed\Plugins($Settings);
@@ -35,13 +53,16 @@ require('Router.php');
 $Router = new Seed\Router($Spyc->loadFile('routes.yml'));
 
 //Route URL
-$url = parse_url($_SERVER['REQUEST_URI']);
-$request = $Router->route($url['path']);
+$request = $Router->route($request['path']);
 
 //Instantiate resource
-$class_name = 'Seed\Resources\\'.$request['resource'];
-$verb = $_SERVER['REQUEST_METHOD'];
-$Resource = new $class_name($Plugins);
+try {
+    $class_name = 'Seed\Resources\\'.$request['resource'];
+    $verb = $_SERVER['REQUEST_METHOD'];
+    $Resource = new $class_name($Plugins);
+} catch (Exception $e) {
+    //TODO: Trigger 404 error message.
+}
 
 //Apply verb to resource (aka. call the method of the resource eg. $resource->get())
 if (method_exists($Resource, $verb)) {
